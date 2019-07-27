c76ce37ff7d05e5a7b995f046f0c4483169535d5
fatih <derefatih@windowslive.com>
2019-07-27 11:38:06 +0300

    adminMenu viewcomponent added

---
+++ BlogApp.WebUI/ViewComponents/AdminLeftMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.WebUI.ViewComponents
+{
+    public class AdminLeftMenuViewComponent:ViewComponent
+    {
+        /*
+         
+          dependency injection ile repositoryden data alıp view'a gönderebiliriz
+             
+             */
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
